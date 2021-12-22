f2baa0781750c6249e8eee99927d512ecc061c10
Randy Fay <randy@randyfay.com>
2021-12-22 08:52:52 -0700

    Just use brew for golang

--- .github/workflows/macos-colima-setup.sh
+++ .github/workflows/macos-colima-setup.sh
@@ -2,9 +2,9 @@
 
 set -eu -o pipefail
 
-brew install docker mkcert mysql-client
+brew install docker golang mkcert mysql-client
 brew install colima --HEAD
-brew link mysql-client
+brew link --overwrite mysql-client golang
 
 # This command allows adding CA (in mkcert, etc) without the popup trust prompt
 # Mentioned in https://github.com/actions/virtual-environments/issues/4519#issuecomment-970202641
