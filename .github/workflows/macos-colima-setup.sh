bb3c61f7c72997b70f1ae25ad5114ec2aa176547
Randy Fay <randy@randyfay.com>
2021-12-22 08:43:43 -0700

    Try working with trusted security

--- .github/workflows/macos-colima-setup.sh
+++ .github/workflows/macos-colima-setup.sh
@@ -2,8 +2,12 @@
 
 set -eu -o pipefail
 
-brew install colima docker mkcert mysql-client
+brew install docker mkcert mysql-client
+brew install colima --HEAD
 brew link mysql-client
-sudo ls
+
+# This command allows adding CA (in mkcert, etc) without the popup trust prompt
+# Mentioned in https://github.com/actions/virtual-environments/issues/4519#issuecomment-970202641
+sudo security authorizationdb write com.apple.trust-settings.admin allow
 colima start
-sudo mkcert -install
+mkcert -install
